5c20bc4adb2a45079f3dbb85efbecf555b14e1dd
Chiemeka Ihebom <emekavilla@gmail.com>
2017-03-04 15:30:56 -0500

    Rails react project

--- client/app/bundles/HelloWorld/components/HelloWorld.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,4 +1,8 @@
 import React, { PropTypes } from 'react';
+import axios from 'axios';
+
+
+
 
 export default class HelloWorld extends React.Component {
   static propTypes = {
@@ -14,32 +18,92 @@ export default class HelloWorld extends React.Component {
 
     // How to set initial state in ES6 class syntax
     // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { name: this.props.name };
+    this.state = {
+       value: '',
+       comments: [],
+    };
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
-  updateName = (name) => {
-    this.setState({ name });
-  };
+
+componentDidMount() {
+    this.getRequest();
+  }
+
+  getRequest() {
+
+    const url = 'https://soccer-arts-blog.firebaseio.com/posts/.json';
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+
+        const data = response.data;
+        let comments = [];
+
+          if (data) {
+            comments = Object.keys(data).map((id) => {
+              const comment = data[id];
+              return {
+                id: id,
+                post: comment.post
+              };
+            });
+          }
+
+
+          comments.reverse();
+
+        this.setState({ comments })
+      })
+
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+ postRequest() {
+    const url = 'https://soccer-arts-blog.firebaseio.com/posts.json';
+
+    axios.post(url, {
+          post: this.state.value
+          })
+          .then(() => {
+            this.getRequest();
+            this.setState({ value: '' })
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+  }
+
+   handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.postRequest();
+  }
 
   render() {
     return (
-      <div>
-        <h3>
-          Hello, {this.state.name}!
-        </h3>
-        <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
-        </form>
-      </div>
+      <Input
+          inputValue={this.state.input}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <div className="Tweets">
+        <Comments
+          id="test"
+          comments={this.state.comments}
+          deleteRequest={this.deleteRequest}
+          patchRequest={this.patchRequest}
+        />
+          </div>
     );
   }
 }
